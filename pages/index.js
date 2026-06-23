e5e45892dc7c51383ff80bbd9e8a7e51cdfafd71
ulissesalvezortiz <48267993+UlissesAlvesOrtiz@users.noreply.github.com>
2026-06-23 23:05:42 +0000

    Treinamentos

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home(){
 
      return <h1> Projeto Engenharia  de Software </h1>
             <h2> Desenvolvimento de Aplicações Web  </h2>
-     
+            <h3>Matemática lógica</h3>
+      
 }
 export default Home;
