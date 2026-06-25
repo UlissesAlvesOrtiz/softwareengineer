1935dadf6820d21273b512ee1e16fdafd2606ba1
ulissesalvezortiz <48267993+UlissesAlvesOrtiz@users.noreply.github.com>
2026-06-25 23:29:50 +0000

    Alteração para deploy no vercel

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ function Home(){
      return <h1> Projeto Engenharia  de Software </h1>
             <h2> Desenvolvimento de Aplicações Web  </h2>
             <h3>Matemática lógica</h3>
-            <p>!!!!!!</p>
             <p>Engenharia de software</p>
      
       
